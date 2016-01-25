21da35ba728571c2be3ae8a6084c4029fbea148b
Steve Shearn <steve@syntronian.com>
2016-01-25 10:15:42 +1000

    revise data model

--- source/bat.data/scripts/1.0.0.0.sql
+++ source/bat.data/scripts/1.0.0.0.sql
@@ -3,8 +3,17 @@ CREATE TABLE [dbo].[Account](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Email] [varchar](300) NOT NULL,
 	[Password] [varchar](128) NOT NULL,
+	[AccountType_ID] [int] NULL,
  CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
 ) 
+
+go
+insert into account
+select 'steve', 'steve', 1
+
+go
+insert into account
+select 'alex', 'alex', 2
---
+++ source/bat.logic/Constants/AccountTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bat.logic.Constants
+{
+    public class AccountTypes
+    {
+        public static int Student = 1;
+        public static int Teacher = 2;
+    }
+}
