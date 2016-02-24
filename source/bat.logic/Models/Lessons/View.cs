598c9517eac78dab9667256264d4fbc2a46e3444
Samuel Eames <sross.eames@gmail.com>
2016-02-24 12:30:37 +1000

    Moved embedded player from meet to Lesson/view.

--- source/bat.logic/Models/Lessons/View.cs
+++ source/bat.logic/Models/Lessons/View.cs
@@ -11,6 +11,11 @@ namespace bat.logic.Models.Lessons
     public class View : Master
     {
         public Lesson lesson { get; set; }
+        public string session { get; set; }
+        public string token { get; set; }
+
+        public Account me { get; set; }
+        public List<Account> others { get; set; }
 
         public View()
         {
@@ -20,6 +25,10 @@ namespace bat.logic.Models.Lessons
                 DurationMins = 15,
                 ClassSize = 1
             };
+
+            this.session = "1_MX40NTQ5NjY1Mn5-MTQ1NjAxMDQ5NjI4MH5MTUNGT3R6RThEVGtxaFJwSTNoRXBpUWd-UH4";
+            this.me = new Account();
+            this.others = new List<Account>();
         }
 
         public bool IsTeacher =>
@@ -31,7 +40,20 @@ namespace bat.logic.Models.Lessons
             {
                 this.lesson = conn.Lessons.FirstOrDefault(l => l.ID == id);
                 if (this.lesson == null) throw new Exception("Lesson does not exist.");
+
+                this.me = this.lesson.Account;
+                foreach (var participant in this.lesson.LessonParticipants.ToList())
+                {
+                    var other = conn.Accounts.FirstOrDefault(a => a.ID == participant.Account_ID);
+                    if (other != null)
+                        this.others.Add(other);
+                }
             }
+
+            if (this.me.Email == "alex")
+                this.token = "T1==cGFydG5lcl9pZD00NTQ5NjY1MiZzaWc9NzZkM2Q1YmM5MWQyZmUzZDcwMzZiZmU4MDQxOTA4MTRmNDk3NmY2Yjpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UUTVOalkxTW41LU1UUTFOakF4TURRNU5qSTRNSDVNVFVOR1QzUjZSVGhFVkd0eGFGSndTVE5vUlhCcFVXZC1VSDQmY3JlYXRlX3RpbWU9MTQ1NjAxMTkyMyZub25jZT0wLjMyMTY5MTkwNTc1NTYxMTQ2JmV4cGlyZV90aW1lPTE0NTg2MDA5ODcmY29ubmVjdGlvbl9kYXRhPQ==";
+            else
+                this.token = "T1==cGFydG5lcl9pZD00NTQ5NjY1MiZzaWc9OGE4OTZjZGM1NGQyZTMzMDI1YmI0NzQ4ODZlZDlhM2Y5ZjFjNDEzNDpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UUTVOalkxTW41LU1UUTFOakF4TURRNU5qSTRNSDVNVFVOR1QzUjZSVGhFVkd0eGFGSndTVE5vUlhCcFVXZC1VSDQmY3JlYXRlX3RpbWU9MTQ1NjAxMTk3OSZub25jZT0wLjg4MzcxMDU4NDg4MDIyMDQmZXhwaXJlX3RpbWU9MTQ1ODYwMDk4NyZjb25uZWN0aW9uX2RhdGE9";
         }
     }
 }
