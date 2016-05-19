04133417eff1be14730fa4c6689fe4926e96f344
Steve Shearn <steve@syntronian.com>
2016-05-20 09:59:33 +1000

    get parent lessons

--- source/bat.logic/ViewModels/Records/Records.cs
+++ source/bat.logic/ViewModels/Records/Records.cs
@@ -19,7 +19,25 @@ namespace bat.logic.ViewModels.Records
                 this.account = conn.Accounts.FirstOrDefault(a => a.ID == id);
                 if (this.account == null) throw new Exception("Account does not exist.");
 
-                this.lessons = conn.Lessons.Where(p => p.Account_ID == id).ToList();              
+
+                var parentLessons = conn.Lessons
+                    .Join(conn.LessonParticipants,
+                        l => l.ID, // lesson primary key
+                        p => p.Lesson_ID, // equals participant foreign key
+                        (l, p) => new {l, p})
+                    .Select(s => new
+                    {
+                        s.l,
+                        participant = s.p.Account_ID
+                    })
+                    .Where(l => l.participant == id);
+
+                foreach (var l in parentLessons.ToList())
+                {
+                    this.lessons.Add(l.l);
+                }
+
+                //this.lessons = conn.Lessons.Where(p => p.Account_ID == id).ToList();              
             }
         }
     }
