782251919104aeb26825074cda442c023f6b694b
Samuel Eames <sross.eames@gmail.com>
2016-05-20 13:37:47 +1000

    Inprogress: Showing Family members lessons in ChatRecords.

--- source/bat.logic/ViewModels/ChatRecords/Index.cs
+++ source/bat.logic/ViewModels/ChatRecords/Index.cs
@@ -11,7 +11,15 @@ namespace bat.logic.ViewModels.ChatRecords
     {
         public Lesson lesson { get; set; }
         public List<Lesson> lessons { get; set; }
+        public List<Lesson> familyLessons { get; set; }
         public List<Lesson> teacherLessons { get; set; }
+        public class FamilyRecord
+        {
+            public Account familyMember { get; set; }
+            public List<Lesson> lessons { get; set; }
+        }
+        
+        public List<FamilyRecord> familyRecords { get; set; } 
 
         public void Load(int id)
         {
@@ -21,6 +29,7 @@ namespace bat.logic.ViewModels.ChatRecords
                 if (this.account == null) throw new Exception("Account does not exist.");
 
                 this.lessons = new List<Lesson>();
+                this.familyRecords = new List<FamilyRecord>();
 
                 var parentLessons = conn.Lessons
                     .Join(conn.LessonParticipants,
@@ -39,7 +48,40 @@ namespace bat.logic.ViewModels.ChatRecords
                     this.lessons.Add(l.l);
                 }
 
-                this.teacherLessons = conn.Lessons.Where(p => p.Account_ID == id).ToList();              
+                this.teacherLessons = conn.Lessons.Where(p => p.Account_ID == id).ToList();
+
+                this.familyMembers = conn.FamilyMembers.Where(p => p.Parent_ID == id).ToList();
+
+                foreach (var familyMember in this.familyMembers.Select(a => new { a.Account_ID }).ToList())
+                {
+                    var lessons = new List<Lesson>();
+
+                    var account = conn.Accounts.FirstOrDefault(a => a.ID == familyMember.Account_ID);
+                    if (this.account == null) throw new Exception("Account does not exist.");
+
+                    var childLessons = conn.Lessons
+                        .Join(conn.LessonParticipants,
+                            l => l.ID, // lesson primary key
+                            p => p.Lesson_ID, // equals participant foreign key
+                            (l, p) => new { l, p })
+                        .Select(s => new
+                        {
+                            s.l,
+                            participant = s.p.Account_ID
+                        })
+                        .Where(l => l.participant == familyMember.Account_ID);
+
+                    foreach (var l in parentLessons.ToList())
+                    {
+                        lessons.Add(l.l);
+                    }
+
+                    this.familyRecords.Add(new FamilyRecord()
+                    {
+                        familyMember = account,
+                        lessons = lessons
+                    });
+                }
             }
         }
     }
