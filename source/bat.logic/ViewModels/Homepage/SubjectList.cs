8b76e0ad6e0b6dedff7700554fcfbc1fa4afe6fe
Samuel Eames <sross.eames@gmail.com>
2016-05-24 12:58:14 +1000

    Modified SubjectList.cs to give the number of Lesson spots left.

--- source/bat.logic/ViewModels/Homepage/SubjectList.cs
+++ source/bat.logic/ViewModels/Homepage/SubjectList.cs
@@ -14,17 +14,39 @@ namespace bat.logic.ViewModels.Homepage
         public List<Lesson> lessons { get; set; }
         public String subject { get; set;}
 
+        public class lesson
+        {
+            public Lesson lessonInfo { get; set; }
+            public int openSlots { get; set; }
+        }
+
+        public List<lesson> subjectLessons { get; set; }
+
         public void Load(string subject)
         {
             var subjectSpace = subject.Replace("_", " ");
 
+            this.subjectLessons = new List<lesson>();
             this.subject = subjectSpace;
 
             using (var conn = new dbEntities())
             {
-                this.lessons = conn.Lessons
+                var lessons = conn.Lessons
                                         .Where(l => l.Subject == subjectSpace && 
                                               (l.ClassSize == 0 || l.ClassSize > l.LessonParticipants.Count)).ToList();
+
+                foreach (var lesson in lessons)
+                {
+
+                    List<LessonParticipant> participants = conn.LessonParticipants
+                                        .Where(l => l.Lesson_ID == lesson.ID).ToList();
+
+                    this.subjectLessons.Add(new lesson
+                    {
+                        lessonInfo = lesson,
+                        openSlots = (lesson.ClassSize - participants.Count())
+                    });
+                }
             }
         }
     }
