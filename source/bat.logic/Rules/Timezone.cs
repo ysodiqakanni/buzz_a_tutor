3c0a31f61e89d49b4d1475fce7b45eca96cbb192
Steve Shearn <steve@syntronian.com>
2016-07-31 18:31:11 +1000

    tidy up

--- source/bat.logic/Rules/Timezone.cs
+++ source/bat.logic/Rules/Timezone.cs
@@ -9,30 +9,24 @@ namespace bat.logic.Rules
 {
     public class Timezone
     {
-        private static TimeZoneInfo tzidGB
-        {
-            get
-            {
-                //// Get timezone id's here: http://stackoverflow.com/questions/5996320/net-timezoneinfo-from-olson-time-zone
-                return TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
-            }
-        }
+        //// Get timezone id's here: http://stackoverflow.com/questions/5996320/net-timezoneinfo-from-olson-time-zone
+        private static string GMT = "GMT Standard Time";
 
-        public static DateTime Now => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzidGB);
+        public static DateTime Now => ConvertFromUTC(DateTime.UtcNow);
 
         public static DateTime Today => new DateTime(Now.Year, Now.Month, Now.Day);
 
         public static DateTime ConvertToUTC(DateTime date)
         {
             return TimeZoneInfo.ConvertTimeBySystemTimeZoneId(
-                Convert.ToDateTime(date.ToString("dd MMM yyyy hh:mm:ss tt")), "GMT Standard Time", "UTC");
+                Convert.ToDateTime(date.ToString("dd MMM yyyy hh:mm:ss tt")), GMT, "UTC");
         }
 
         public static DateTime? ConvertToUTC(DateTime? date) => date.HasValue ? ConvertToUTC(date.Value) : (DateTime?)null;
 
         public static DateTime ConvertFromUTC(DateTime date)
         {
-            return TimeZoneInfo.ConvertTimeFromUtc(date, tzidGB);
+            return TimeZoneInfo.ConvertTimeFromUtc(date, TimeZoneInfo.FindSystemTimeZoneById(GMT));
         }
 
         public static DateTime? ConvertFromUTC(DateTime? date) => date.HasValue ? ConvertFromUTC(date.Value) : (DateTime?)null;
