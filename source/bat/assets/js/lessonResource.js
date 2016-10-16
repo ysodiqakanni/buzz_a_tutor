b32780de9e0a6ebc5a589e061cd084cf99122031
Samuel Eames <sross.eames@gmail.com>
2016-10-16 11:54:40 +1000

    Pdf pages now added to carousel and carousel controls work.

--- source/bat/assets/js/lessonResource.js
+++ source/bat/assets/js/lessonResource.js
@@ -17,6 +17,7 @@ $("#ClassResource").change(function (event) {
 
 function loadPreview() {
     $('#toImageModal').modal('hide');
+    $('#canvasCarousel').empty();
     var fileName = file.name;
     var fileExt = fileName.split('.').pop().toLowerCase();
     if (fileExt == "pdf") {
@@ -26,54 +27,61 @@ function loadPreview() {
             //Step 4:turn array buffer into typed array
             var typedarray = new Uint8Array(this.result);
             //Step 5:PDFJS should be able to read this
+            var currPage = 1; //Pages are 1-based not 0-based
+            var numPages = 0;
+            var thePDF = null;
             PDFJS.getDocument(typedarray).then(function (pdf) {
-                function createCanvas(num) {
-                    console.log(num);
-                    canvasId = "previewCanvas-" + num;
-                    if (num == 1) {
-                        $('#canvasCarousel').append('<div class="item active"><canvas id="'+canvasId+'"></canvas><div class="carousel-caption"></div></div>');
-                    } else {
-                        $('#canvasCarousel').append('<div class="item"><canvas id='+canvasId+'"></canvas><div class="carousel-caption"></div></div>');
-                    }
-                }
-                function getPage(pagenum) {
-                    console.log(pagenum);
-                    pdf.getPage(pagenum)
-                }
-                for(var i = 1; i <= pdf.numPages; i++) {
-                    console.log('Number of Pages: ' + i);
-                    $.when(createCanvas(i))
-                    .then(pdf.getPage(i)
-                    .then(function getPage(page) {
-                        //
-                        // Prepare canvas using PDF page dimensions
-                        //
-                        canvasId = "previewCanvas-" + pagenum;
-                        console.log(canvasId);
-                        var canvas = document.getElementById(canvasId);
-                        var scale = 0.8;
-                        var context = canvas.getContext('2d');
-                        var viewport = page.getViewport(scale);
-                        canvas.width = viewport.width;
-                        canvas.height = viewport.height;
-                        //
-                        // Render PDF page into canvas context
-                        //
-                        var renderContext = {
-                            canvasContext: context,
-                            viewport: viewport
-                        };
-                        page.render(renderContext);
-                        $('#previewTitle').val(fileName);
-                        $('#previewModal').modal();
-                    }))
-                    .then(function () {
-                        console.log("Requests succeeded and animations completed");
-                    }).fail(function () {
-                        console.log("something went wrong!");
-                    })
-                }
+                //Set PDFJS global object (so we can easily access in our page functions
+                thePDF = pdf;
+
+                //How many pages it has
+                numPages = pdf.numPages;
+
+                //Start with first page
+                pdf.getPage(1).then(handlePages);
             });
+
+            function handlePages(page) {
+                //This gives us the page's dimensions at full scale
+                var viewport = page.getViewport(1);
+
+                //We'll create a canvas for each page to draw it on
+                var id = 'carousel' + currPage;
+                if (currPage == 1) {
+                    $('#canvasCarousel').append('<div id="' + id + '"class="item active"></div>');
+                } else {
+                    $('#canvasCarousel').append('<div id="' + id + '"class="item"></div>');
+                }
+                var carouselItem = document.getElementById(id)
+
+                var canvas = document.createElement("canvas");
+                canvas.style.display = "block";
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                //Draw it on the canvas
+                page.render({ canvasContext: context, viewport: viewport });
+
+                //Add it to the web page
+                carouselItem.appendChild(canvas);
+                $('#previewTitle').val(fileName);
+
+                if (currPage == numPages) {
+                    $("#preview-carousel").append('<a class="left carousel-control" href="#preview-carousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a>');
+                    $("#preview-carousel").append('<a class="right carousel-control" href="#preview-carousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>');
+                    $('#preview-carousel').carousel();
+                }
+
+                $('#previewModal').modal();
+
+                //Move to next page
+
+                currPage++;
+                if (thePDF !== null && currPage <= numPages) {
+                    thePDF.getPage(currPage).then(handlePages);
+                }
+            }
         }
         //Step 3:Read the file as ArrayBuffer
         fileReader.readAsArrayBuffer(file);
