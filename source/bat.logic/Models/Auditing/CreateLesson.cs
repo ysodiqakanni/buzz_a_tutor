20f637b839da3b279937a6e19f46c1573f47d94a
Steve Shearn <steve@syntronian.com>
2016-10-25 18:54:50 +1000

    create lesson model

---
+++ source/bat.logic/Models/Auditing/CreateLesson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bat.logic.Models.Auditing
+{
+    public class CreateLesson
+    {
+        public int ID { get; set; }
+        public int Account_ID { get; set; }
+        public string Description { get; set; }
+        public static string TypeToString()
+        {
+            return "Create Lesson";
+        }
+    }
+}
