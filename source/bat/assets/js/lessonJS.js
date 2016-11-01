69b56299ff59cb1b54fa044b6dd6a23699e8d2b4
Samuel Eames <sross.eames@gmail.com>
2016-11-01 21:09:31 +1000

    Added convert pdf to upload image to lesson page.

--- source/bat/assets/js/lessonJS.js
+++ source/bat/assets/js/lessonJS.js
@@ -24,10 +24,14 @@ $("#bbImageInput").change(function (event) {
     if (fileExt == "png" || fileExt == "jpg" || fileExt == "tif") {
         var fileReader = new FileReader();
         fileReader.addEventListener("load", function () {
+            var currPage = 1; //Pages are 1-based not 0-based
+            var id = "p" + 1;
+            $('#canvasCarousel').append('<div id="' + id + '"class="item active"></div>');
+            var carouselItem = document.getElementById(id)
             var canvas = document.createElement("canvas");
             canvas.style.display = "block";
             var context = canvas.getContext('2d');
-            canvas.id = 'uploadCanvas';
+            canvas.id = "p" + currPage + "-canvas";
             canvas.height = 568;
             canvas.width = 600;
 
@@ -38,11 +42,78 @@ $("#bbImageInput").change(function (event) {
                 context.drawImage(img, 0, 0, canvas.width, canvas.height);
             };
 
-            $("#upload-Canvas").append(canvas);
+            carouselItem.appendChild(canvas);
+            $('#' + id).append('<div class="carousel-caption"><input class="hidden" id="' + id + '-check" type="checkbox" checked/></div');
         }, false);
 
         //Step 3:Read the file as ArrayBuffer
         fileReader.readAsDataURL(imageFile);
+        $("#modal-button-container").append(uploadBtn);
+    } else if (fileExt == "pdf") {
+        //Step 2: Read the file using file reader
+        var fileReader = new FileReader();
+        fileReader.onload = function () {
+            //Step 4:turn array buffer into typed array
+            var typedarray = new Uint8Array(this.result);
+            //Step 5:PDFJS should be able to read this
+            var currPage = 1; //Pages are 1-based not 0-based
+            var numPages = 0;
+            var thePDF = null;
+            PDFJS.getDocument(typedarray).then(function (pdf) {
+                //Set PDFJS global object (so we can easily access in our page functions
+                thePDF = pdf;
+
+                //How many pages it has
+                numPages = pdf.numPages;
+
+                //Start with first page
+                pdf.getPage(1).then(handlePages);
+            });
+
+            function handlePages(page) {
+                //This gives us the page's dimensions at full scale
+                var viewport = page.getViewport(1);
+
+                //We'll create a canvas for each page to draw it on
+                var id = 'p' + currPage;
+                if (currPage == 1) {
+                    $('#canvasCarousel').append('<div id="' + id + '"class="item active"></div>');
+                } else {
+                    $('#canvasCarousel').append('<div id="' + id + '"class="item"></div>');
+                }
+
+                var carouselItem = document.getElementById(id)
+
+                var canvas = document.createElement("canvas");
+                canvas.id = "p" + currPage + "-canvas";
+                canvas.style.display = "block";
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                //Draw it on the canvas
+                page.render({ canvasContext: context, viewport: viewport });
+
+                //Add it to the web page
+                carouselItem.appendChild(canvas);
+                $('#' + id).append('<div class="carousel-caption"><input id="' + id + '-check" type="checkbox"/></div');
+
+                if (currPage == numPages) {
+                    $("#preview-carousel").append('<a class="left carousel-control" href="#preview-carousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a>');
+                    $("#preview-carousel").append('<a class="right carousel-control" href="#preview-carousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>');
+                    $('#preview-carousel').carousel();
+                }
+
+                //Move to next page
+
+                currPage++;
+                if (thePDF !== null && currPage <= numPages) {
+                    thePDF.getPage(currPage).then(handlePages);
+                }
+            }
+        }
+        //Step 3:Read the file as ArrayBuffer
+        fileReader.readAsArrayBuffer(imageFile);
         $("#modal-button-container").append(uploadBtn);
     } else {
         errorMessage = "Incorrect file type.";
@@ -52,29 +123,50 @@ $("#bbImageInput").change(function (event) {
 function uploadImage() {
     $("#modal-button-container").empty();
     $("#modal-button-container").append(uploadingIcon);
-    var img2SaveRaw = document.getElementById('uploadCanvas').toDataURL('image/png'),
-    img2SaveArray = img2SaveRaw.split(','),
-    img2Save = img2SaveArray[1],
-    title = imageFile.name;
-    $.ajax({
-        type: "POST", // Type of request
-        url: "../api/lessons/uploadtocloud", //"../api/lessons/upload", //The controller/Action
-        dataType: "json",
-        data: {
-            "lessonid": lessonId,
-            "title": title,
-            "data": img2Save,
-        },
+    var page = 1;
+    var pages = $("#canvasCarousel .item").length;
+    function postCanvas(id) {
+        var canvas = document.getElementById(id + "-canvas");
+        var img2SaveRaw = canvas.toDataURL('image/png'),
+        img2SaveArray = img2SaveRaw.split(','),
+        img2Save = img2SaveArray[1],
+        title = imageFile.name;
+        $.ajax({
+            type: "POST", // Type of request
+            url: "../api/lessons/uploadtocloud", //"../api/lessons/upload", //The controller/Action
+            dataType: "json",
+            data: {
+                "lessonid": lessonId,
+                "title": title + "-" + id,
+                "data": img2Save,
+            },
 
-        success: function (data) {
+            success: function (data) {
+                console.log("success");
+            },
+
+            error: function (err) {
+                errorMessage = "Something went wrong, try again later.";
+                $("#bbImageError").append(errorStart + errorMessage + errorEnd);
+                console.log("error[" + err.status + "]: " + err.statusText);
+            }
+        })
+    }
+    function ifchecked() {
+        var id = 'p' + page;
+        var inputId = id + "-check";
+        if ($("#" + inputId).is(":checked")) {
+            postCanvas(id);
+        }
+        page++
+        if (page <= pages) {
+            ifchecked();
+        } else if (page > pages) {
             $("#modal-button-container").empty();
             $("#modal-button-container").append(successBtn);
-        },
-
-        error: function (err) {
-            errorMessage = "Something went wrong, try again later.";
-            $("#bbImageError").append(errorStart + errorMessage + errorEnd);
-            console.log("error[" + err.status + "]: " + err.statusText);
         }
-    })
+    }
+    if (page < pages || page == pages) {
+        ifchecked();
+    }
 }
